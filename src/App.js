4e5c20eb41a35712f15d2204d70c96bedc0722aa
spiism <sp.iism@gmail.com>
2022-01-11 10:45:39 +1300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import THANK_U_NEXT from "./THANK_U_NEXT";
+import "./styles.css";
 
-function App() {
+const getFilteredItems = (query, items) => {
+  if (!query) {
+    return items;
+  }
+  return items.filter((song) => song.name.includes(query));
+};
+
+export default function App() {
+  const [query, setQuery] = useState("");
+
+  const { tracks } = THANK_U_NEXT;
+  const { items } = tracks;
+  // items looks like this: [{name: 'name1'}, {name: 'name2'}]
+
+  const filteredItems = getFilteredItems(query, items);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Search</label>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      <ul>
+        {filteredItems.map((value) => (
+          <h1 key={value.name}>{value.name}</h1>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
